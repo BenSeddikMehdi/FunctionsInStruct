884f65ba791af1991cff13f245301237a5bd70bb
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-11 15:51:57 +0100

    Trying to create a variable of type struct and can access a function inside that struct

---
+++ StructWithFunctions.h
@@ -0,0 +1,27 @@
+//
+// Created by El Mehdi Benseddik on 12/11/2019.
+//
+
+#ifndef FUNCTIONSINSTRUCT_STRUCTWITHFUNCTIONS_H
+#define FUNCTIONSINSTRUCT_STRUCTWITHFUNCTIONS_H
+
+typedef struct newStruct{
+    int a;
+    int (*length) ();
+}list_t;
+
+int length() {
+    int i = 0;
+    while (a > 0) {
+        a /= 10;
+        i++;
+    }
+    return i;
+}
+list_t b;
+int print() {
+    b.a = 1234;
+    int k = b.length();
+}
+list_t newVar = {134, 0};
+#endif //FUNCTIONSINSTRUCT_STRUCTWITHFUNCTIONS_H
